f818b0c745d76a5d3cad09460beba943ff59997b
Amanuel Ayalew <amanu.ayalew@gmail.com>
2020-08-13 19:41:33 -0700

    First Day Assignment

---
+++ Hello_World.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("I want to go to college at MIT.");
+        System.out.println("I would like to major in Software Engineering.");
+        System.out.println("My favorite hobbies are playing and making music, developing games, and playing games (Video games and sports).");
+        System.out.println("My favorite fruits are pineapples and mangoes.");
+        System.out.println("My favorite food is chutoro sushi and my favorite drink is iced milk tea.");
+    }
+}
